a0a0df8da37a5e03e3d4de6ebbd0ee7a15819339
Kunal Garg <imgargk18@gmail.com>
2024-06-16 13:56:07 +0530

    Initialized projects in vite and basic react

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from './chai'
+
+function App() {
+  return (
+    <> 
+    {/*  fragment in react, we can't return multiple components */}
+    <Chai/> {/* Name needs to be captialized */}
+    <h1>Chai aur React</h1>
+    </>
+  );
+}
+export default App
+
