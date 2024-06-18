f6df602be891b4ce223c16592849af3388cc9643
Kunal Garg <imgargk18@gmail.com>
2024-06-18 23:09:23 +0530

    react router dom working

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch("https://api.github.com/users/kunalgarg-18")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white
+     p-4 text-3xl '>
+      Github Followers: {data.followers}
+      <img className='float: auto; width: auto justify-center' src={data.avatar_url} alt="Git Picture" width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async() => {
+    const response = await fetch('https://api.github.com/users/kunalgarg-18')
+    return response.json()
+}
